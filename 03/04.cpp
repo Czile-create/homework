cdcfc6040e86d7a71c2490a0d98d7d41ac27270c
Czile <czile@foxmail.com>
2021-04-07 14:44:29 +0800

    update 03/04.cpp. fix a bug like (-2)*(-3)

--- 03/04.cpp
+++ 03/04.cpp
@@ -9,41 +9,39 @@ double ans(string s) {
     smatch ex;
 
     // process the operator ()
-    while (regex_search(s.cbegin(), s.cend(), ex, regex("\\((.*?)\\)"))) {           
+    while (regex_search(s.cbegin(), s.cend(), ex, regex("\\((\\d|\\+|-|\\*|/|\\.)*?\\)"))) {           
         auto t = string(ex[0].first+1, ex[0].second-1);
         s.replace(s.find(ex[0]), ex[0].length(), to_string(ans(t)));
     }
 
     // To check if the expreesion is true.
     // NOTE: Not finished.
-    if (!regex_match(s, regex("(\\d|\\+|-|\\*|/|\\.)*")))
+    if (!regex_match(s, regex("(\\d|\\+|-|\\*|/|\\.)*"))) {
+        cerr<<s;
         throw "Expression Error!\n";
+    }
 
     // process the operator [*/]
-    while (regex_search(s.cbegin(), s.cend(), ex, regex("(\\d+(\\.\\d+)?)([\\*/])(\\d+(\\.\\d+)?)"))) {
+    while (regex_search(s.cbegin(), s.cend(), ex, regex("(-?\\d+(\\.\\d+)?)([\\*/])(-?\\d+(\\.\\d+)?)"))) {
         smatch s1, s2, s3;
-        regex_search(ex[0].first, ex[0].second, s1, regex("\\d+(\\.\\d+)?"));   // first number
-        regex_search(s1[0].second, ex[0].second, s2, regex("\\*|/"));           // operator
-        regex_search(s2[0].second, ex[0].second, s3, regex("\\d+(\\.\\d+)?"));  // second number
+        regex_search(ex[0].first, ex[0].second, s1, regex("-?\\d+(\\.\\d+)?"));     // first number
+        regex_search(s1[0].second, ex[0].second, s2, regex("\\*|/"));               // operator
+        regex_search(s2[0].second, ex[0].second, s3, regex("-?\\d+(\\.\\d+)?"));    // second number
         if (s2[0] == "*")
-            s.replace(s.find(ex[0]), ex[0].length(), to_string(stod(s1[0])*stod(s3[0])));
+            s.replace(s.find(ex[0]), ex[0].length(), "+"+to_string(stod(s1[0])*stod(s3[0])));
         else
-            s.replace(s.find(ex[0]), ex[0].length(), to_string(stod(s1[0])/stod(s3[0])));
+            s.replace(s.find(ex[0]), ex[0].length(), "+"+to_string(stod(s1[0])/stod(s3[0])));
     }
 
     // process the operator [+-]
-    while (regex_search(s.cbegin(), s.cend(), ex, regex("(\\d+(\\.\\d+)?)([\\+-])(\\d+(\\.\\d+)?)"))) {
-        smatch s1, s2, s3;
-        regex_search(ex[0].first, ex[0].second, s1, regex("\\d+(\\.\\d+)?"));   // first number
-        regex_search(s1[0].second, ex[0].second, s2, regex("\\+|-"));           // operator
-        regex_search(s2[0].second, ex[0].second, s3, regex("\\d+(\\.\\d+)?"));  // second number
-        if (s2[0] == "+")
-            s.replace(s.find(ex[0]), ex[0].length(), to_string(stod(s1[0])+stod(s3[0])));
-        else
-            s.replace(s.find(ex[0]), ex[0].length(), to_string(stod(s1[0])-stod(s3[0])));
+    double answer = 0;
+    auto first = s.cbegin(), last = s.cend();
+    while (regex_search(first, last, ex, regex("-?\\d+(\\.\\d+)?"))) {
+        answer += stod(ex[0]);
+        first = ex[0].second;
     }
 
-    return stod(s);
+    return answer;
 }
 
 int main() {
